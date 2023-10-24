1c7876612b8bf08af37727334bd783163241c0d8
DidasMba <daniel.mbarushi@gmail.com>
2023-10-24 23:34:10 +0300

    first testing react

--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function MyComponent(){
+    return <div className=""> Hu</div>
+}
+
+function Article() {
+    return (
+      <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+    );
+  }
+
+ReactDOM.render(<div>
+    <Article/>
+</div>, document.getElementById("root"));
